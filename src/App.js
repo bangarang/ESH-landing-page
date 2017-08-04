73c293b74268e80a9646a28fe561aa438740318c
pollman <caleb.pollman@zeeram.com>
2017-08-04 15:14:53 -0600

    created app vars, background css added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Background from './components/Background';
+import Main from './components/Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Background />
+        <Main />
       </div>
     );
   }
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './Footer';
+
+const Main = () => {
+  return (
+    <div className="main-background background">
+      <div className="top-background background"></div>
+      <div className="middle-background background"></div>
+      <div className="bottom-background background"></div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
